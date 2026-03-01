fa36d9d8248dc2d3b52293bb87b456aecf8f6fba
Daniil Ruban (AQABlatter) <itblatter@gmail.com>
2026-03-01 18:32:18 +0500

    added TestBase

---
+++ src/test/java/pages/PagesAutoTrust.java
@@ -0,0 +1,28 @@
+package pages;
+
+
+import com.codeborne.selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import io.qameta.allure.Step;
+import org.openqa.selenium.devtools.v85.page.Page;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PagesAutoTrust {
+}
+        private final SelenideElement
+            autoBanner = $(".e5uq2lb4.css-1x7bbe9.e4jennr0"),
+            loginForm = $(".css-zmducx.ez92ydl0"),
+            loginString = $("#auth-form-login"),
+            passwordString = $("#auth-form-password"),
+            hidenButton = (".css-otmbbu.est9fan1"),
+            rememberButton (".css-17h2mrt.eh443m60"),
+            wronngPasswordButton (".css-17h2mrt eh443m60"),
+            entryButton (".css-17h2mrt.eh443m60"),
+            aTrustButton (".css-d2v28b.eh443m60"),
+            helpButton ("#login-helpers-icon"),
+
+
+
+
+
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,47 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+
+
+import java.util.List;
+import java.util.Map;
+
+import static helpers.Attach.*;
+
+public class TestBase {
+
+    @BeforeAll // выполняет настройки которые в ней заданы ПЕРЕД ВСЕМИ тестами один раз
+    static void beforeAll() {
+        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager"; //стратегия загрузки, не дожидаемся полной загрузки
+        Configuration.baseUrl = "https://www.mvideo.ru/"; // выносим основной сайт
+        Configuration.holdBrowserOpen = false;
+
+
+    }
+
+    @BeforeEach //метод выполняется перед каждым тестом
+    void setUpBeforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()); //включение слушателя Аллюр
+
+    }
+    @AfterEach
+        //выполняется после каждого теста
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot"); //Скриншот последнего состояния браузера.
+        Attach.pageSource(); //Исходный код страницы.
+        Attach.browserConsoleLogs(); //Логи консоли браузера.
+        Attach.addVideo(); //Видео записи теста.
+        Selenide.closeWebDriver(); //Закрывает браузер.
+    }
+}
